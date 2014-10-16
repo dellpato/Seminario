e8a3d5b6babaebcf865f8dd034a14dc14288692c
Luis Cordón <r2lac.28@gmail.com>
2014-10-16 10:37:08 -0600

    mi primer git
    jajajjaa

---
+++ SysKar/connection.php
@@ -0,0 +1,13 @@
+//Programa para la conexion hacia la base de datos
+
+<?php
+
+	$host='localhost';
+	$user='';
+	$pass='';
+	$bdata='';
+	
+	$dbconn = new mysqli($host,$user,$pass,$bdata);
+	
+	
+?>
